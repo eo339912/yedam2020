71f4c953042140189fe4a6f453eaee41ace58bf7
User <User@YD02-11>
2020-01-21 12:48:48 +0900

    asd

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello, Welcome to the java world!");
+		int age = 30;
+		System.out.println("³ªÀÌ´Â " + age);
+	}
+}
